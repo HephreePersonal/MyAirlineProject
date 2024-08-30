6224c5890e25d280b14635cac5b8cbf7d2e39dc8
Jeffrey Reichert <torres9fan@gmail.com>
2024-08-30 12:53:29 -0600

    yo catching up

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
 <?php
 require_once 'database/db_connect.php';
 require_once 'airlines/Airlines_Grid.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
 
 // Start the HTML
 require_once 'public/header.php';
